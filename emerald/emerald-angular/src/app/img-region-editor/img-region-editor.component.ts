d1808c04a30237752618aab23687f69984d3bd29
Sergei Zykov <sergey.zykov@gmail.com>
2017-08-21 01:11:15 +0300

    Client rotation - done

--- emerald/emerald-angular/src/app/img-region-editor/img-region-editor.component.ts
+++ emerald/emerald-angular/src/app/img-region-editor/img-region-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ITreeNode, NodeType } from '../emerald-backend-storage.service'
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-img-region-editor',
@@ -8,7 +9,37 @@ import { ITreeNode, NodeType } from '../emerald-backend-storage.service'
 })
 export class ImgRegionEditorComponent {
   @Input() SelectedNode: ITreeNode;
+  @Input() Rot: string;
+  private angle : number = 0;
 
   constructor() {
+    this.Rot = this.convAngle(this.angle);
+  }
+
+  rotateCW(event:any): void {
+    this.Rot = this.convAngle(++this.angle);
+  }
+
+  rotateCCW(event:any): void {
+    this.Rot = this.convAngle(--this.angle);
+  }
+
+  private convAngle(arg: number) : string {
+    switch (arg % 4) {
+      case -1:
+        return 'CCW90';
+      case -2:
+        return 'CCW180';
+      case -3:
+        return 'CCW270';
+      case 1:
+        return 'CW90';
+      case 2:
+        return 'CW180';
+      case 3:
+        return 'CW270';
+      default:
+        return 'NONE';
+    }
   }
 }
