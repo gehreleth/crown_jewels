1be27a127dbaf3ddc7e9ea4b429a33c0a31dd9dd
Sergei Zykov <sergey.zykov@gmail.com>
2017-08-23 17:31:05 +0300

    Removed unused imports

--- emerald/emerald-angular/src/app/img-region-editor.service.ts
+++ emerald/emerald-angular/src/app/img-region-editor.service.ts
@@ -3,14 +3,7 @@ import { ITreeNode, NodeType } from './emerald-backend-storage.service'
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/take'
 
 export enum Rotation {
   NONE = 0,
