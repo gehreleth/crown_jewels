f6c86869672dd9880897604e8e99364025215a53
Sergei Zykov <sergey.zykov@gmail.com>
2017-09-07 22:52:47 +0300

    Reduced code a little

--- emerald/emerald-angular/src/app/ire-main-area/ire-main-area.component.ts
+++ emerald/emerald-angular/src/app/ire-main-area/ire-main-area.component.ts
@@ -4,51 +4,62 @@ import { DomSanitizer, SafeUrl, SafeStyle } from '@angular/platform-browser';
 import { IImageMeta, Rotation } from '../image-meta';
 
 const HandlerSize: number = 8;
-enum HP { NW, N, NE, W, E, SW, S, SE };
-interface Coords { 'left':number, 'top':number };
+enum PrincipialWind { NW, N, NE, W, E, SW, S, SE };
+interface HandlerStyles { 'left': number, 'top': number, 'cursor': string };
 
-function rhc(show: HP, area: any): Coords {
+function rhc(show: PrincipialWind, area: any): HandlerStyles {
   let top: number;
   let left: number;
+  let pw: string;
   let semiwidth = Math.round(HandlerSize / 2);
   let semiheight = Math.round(HandlerSize / 2);
   switch(show) {
-    case HP.NW:
+    case PrincipialWind.NW:
       top = -semiheight;
       left = -semiwidth;
+      pw = 'nw-resize';
       break;
-    case HP.N:
+    case PrincipialWind.N:
       top = -semiheight;
       left = Math.round(area.width / 2) - semiwidth - 1;
+      pw = 'n-resize';
       break;
-    case HP.NE:
+    case PrincipialWind.NE:
       top = -semiheight;
       left = area.width - semiwidth - 1;
+      pw = 'ne-resize';
       break;
-    case HP.W:
+    case PrincipialWind.W:
       top = Math.round(area.height / 2) - semiheight - 1;
       left = -semiwidth;
+      pw = 'w-resize';
       break;
-    case HP.E:
+    case PrincipialWind.E:
       top = Math.round(area.height / 2) - semiheight - 1;
       left = area.width - semiwidth - 1;
+      pw = 'e-resize';
       break;
-    case HP.SW:
+    case PrincipialWind.SW:
       top = area.height - semiheight - 1;
       left = -semiwidth;
+      pw = 'sw-resize';
       break;
-    case HP.S:
+    case PrincipialWind.S:
       top = area.height - semiheight - 1;
       left = Math.round(area.width / 2) - semiwidth - 1;
+      pw = 's-resize';
       break;
-    case HP.SE:
+    case PrincipialWind.SE:
       top = area.height - semiheight - 1;
       left = area.width - semiwidth - 1;
+      pw = 'se-resize';
       break;
     default:
       throw "Bad case";
   }
-  const retVal: Coords = { left: area.x + left, top: area.y + top }
+  const retVal: HandlerStyles = { 'left': area.x + left,
+  'top': area.y + top,
+  'cursor': pw }
   return retVal;
 }
 
@@ -58,6 +69,7 @@ function rhc(show: HP, area: any): Coords {
   styleUrls: ['./ire-main-area.component.scss']
 })
 export class IreMainAreaComponent {
+  public principialWind = PrincipialWind;
   @Input() Areas: Array<any>;
   @Output() AreasChanged: EventEmitter<Array<any>> = new EventEmitter<Array<any>>();
 
@@ -70,17 +82,17 @@ export class IreMainAreaComponent {
 
   constructor(private _sanitizer: DomSanitizer) { }
 
-  topLevelStyle(): any {
+  private topLevelStyles(): any {
     return {   'position': 'relative',
                'width': `${this.Width}px`,
                'height': `${this.Height}px` };
   }
 
-  imgStyle(): any {
+  private imgStyles(): any {
     return {   'position': 'absolute' };
   }
 
-  overlayStyle(): any {
+  private overlayStyles(): any {
     return {   'opacity': 0.5,
                'position': 'absolute',
                'width': `${this.Width}px`,
@@ -88,7 +100,7 @@ export class IreMainAreaComponent {
                'display': 'block' };
   }
 
-  backgroundStyle(): any {
+  private backgroundStyles(): any {
     return {   'background-color': 'rgb(0, 0, 0)',
                'opacity': 0,
                'position': 'absolute',
@@ -97,7 +109,7 @@ export class IreMainAreaComponent {
                'cursor': 'crosshair' };
   }
 
-  outlineStyle(area: any): any {
+  private outlineStyles(area: any): any {
     return {   'opacity': 0.5,
                'position': 'absolute',
                'cursor': 'default',
@@ -108,7 +120,7 @@ export class IreMainAreaComponent {
                'z-index': 0 };
   }
 
-  sanitizedAreaBackground(area: any) : SafeStyle {
+  private sanitizedAreaBackgroundStyles(area: any) : SafeStyle {
     let url = this._sanitizer.sanitize(SecurityContext.URL, this.ImageHref);
     return this._sanitizer.bypassSecurityTrustStyle('rgb(255, 255, 255)'
       + ` url("${url}")`
@@ -116,7 +128,7 @@ export class IreMainAreaComponent {
       + `/ ${this.Width}px ${this.Height}px`);
   }
 
-  areaBackgroundOther(area: any): any {
+  private areaBackgroundOtherStyles(area: any): any {
     return {   'position': 'absolute',
                'cursor': 'move',
                'width': `${area.width - 2}px`,
@@ -126,96 +138,20 @@ export class IreMainAreaComponent {
                'z-index': 2 };
   }
 
-  areaResizeHandlerW(area: any): any {
-    const pos = rhc(HP.W, area);
+  private areaResizeHandlerStyles(area: any, pw: PrincipialWind): any {
+    const hs = rhc(pw, area);
     return {   'opacity': 0.5,
                'position': 'absolute',
-               'cursor': 'w-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
+               'cursor': hs.cursor,
+               'left': `${hs.left}px`,
+               'top': `${hs.top}px`,
                'z-index': 101 };
   }
 
-  // width: 60px; height: 100px; left: 10px; top: 20px;
-  // 6px; top: 115px;
-  areaResizeHandlerSW(area: any): any {
-    const pos = rhc(HP.SW, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'sw-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  // width: 60px; height: 100px; left: 10px; top: 20px;
-  // 35px; top: 115px;
-  areaResizeHandlerS(area: any): any {
-    const pos = rhc(HP.S, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 's-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  // width: 60px; height: 100px; left: 10px; top: 20px;
-  // 35px; top: 115px;
-  areaResizeHandlerSE(area: any): any {
-    const pos = rhc(HP.SE, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'se-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  areaResizeHandlerE(area: any): any {
-    const pos = rhc(HP.E, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'e-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  areaResizeHandlerNE(area: any): any {
-    const pos = rhc(HP.NE, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'ne-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  areaResizeHandlerN(area: any): any {
-    const pos = rhc(HP.N, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'n-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  areaResizeHandlerNW(area: any): any {
-    const pos = rhc(HP.NW, area);
-    return {   'opacity': 0.5,
-               'position': 'absolute',
-               'cursor': 'nw-resize',
-               'left': `${pos.left}px`,
-               'top': `${pos.top}px`,
-               'z-index': 101 };
-  }
-
-  areaDelete(area: any): any {
-    const pos = rhc(HP.NE, area);
-    return {   'left': `${pos.left + 4}px`,
-               'top': `${pos.top - 22}px`,
+  private areaDeleteStyles(area: any): any {
+    const hs = rhc(PrincipialWind.NE, area);
+    return {   'left': `${hs.left + 4}px`,
+               'top': `${hs.top - 22}px`,
                'position': 'absolute',
                'z-index': 1 };
   }
