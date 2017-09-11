9a3c3ea24da410d482f5e629cbf4bd41549021fb
Sergei Zykov <sergey.zykov@gmail.com>
2017-09-11 15:00:26 +0300

    Add new | Move | Select works

--- emerald/emerald-angular/src/app/ire-main-area/ire-main-area.component.ts
+++ emerald/emerald-angular/src/app/ire-main-area/ire-main-area.component.ts
@@ -6,6 +6,20 @@ import { Action } from '../ire-main-area-action-layer/ire-main-area-action-layer
 import { IHandleMouseDown } from '../ire-main-area-handlers/ire-main-area-handlers.component';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+interface IActionContext {
+  action: Action, selection: number, area?: any, originatingEvent?: any
+}
+
+namespace IActionContext {
+  export function initial() : IActionContext {
+    const retVal : IActionContext = {
+      action: Action.NoAction,
+      selection: -1
+    }
+    return retVal;
+  }
+}
+
 @Component({
   selector: 'app-ire-main-area',
   templateUrl: './ire-main-area.component.html',
@@ -26,47 +40,170 @@ export class IreMainAreaComponent {
   private readonly ScaleSE = Action.ScaleSE;
 
   @Input() areas: Array<any>;
-  @Output() areasChanged: EventEmitter< Array<any> > = new EventEmitter< Array<any> >();
+  @Output() areasChanged: EventEmitter< Array<any> > =
+    new EventEmitter< Array<any> >();
 
   @Input() selectedArea: number = 0;
-  @Output() selectedAreaChanged: EventEmitter<number> = new EventEmitter<number>();
+  @Output() selectedAreaChanged: EventEmitter<number> =
+    new EventEmitter<number>();
 
   @Input() imageHref: SafeUrl;
   @Input() width: number;
   @Input() height: number;
 
-  private currentActionSubj: BehaviorSubject<Action> = new BehaviorSubject<Action>(Action.NoAction);
+  private readonly currentActionSubj: BehaviorSubject<IActionContext> =
+    new BehaviorSubject<IActionContext>(IActionContext.initial());
 
   private onOutsideSelectionMouseDown(event: any): void {
-    this.currentActionSubj.next(Action.Add);
+    let newArea = {
+      x: event.layerX, y: event.layerY, width: 100, height: 100
+    }
+    this.areas = this.areas.concat([newArea]);
+    this.areasChanged.emit(this.areas);
+    const selection = this.areas.length - 1;
+    this.selectedArea = selection;
+    this.selectedAreaChanged.emit(this.selectedArea);
+    const actionContext: IActionContext = {
+      action: Action.Add,
+      selection: selection,
+      area: { ...newArea },
+      originatingEvent: event
+    }
+    this.currentActionSubj.next(actionContext);
   }
 
   private onSelectionMouseDown(event: any, ix: number): void {
-    this.currentActionSubj.next(Action.Select);
+    this.selectedArea = ix;
+    this.selectedAreaChanged.emit(this.selectedArea);
+    const area = this.areas[this.selectedArea];
+    const actionContext: IActionContext = {
+      action: Action.Select,
+      selection: this.selectedArea,
+      area: { ...area },
+      originatingEvent: event
+    }
+    this.currentActionSubj.next(actionContext);
   }
 
   private onHandlerMouseDown(event: any, ix: number): void {
-    const event0 = event as IHandleMouseDown;
-    this.currentActionSubj.next(Action.ScaleNW);
+    const handleMouseDown = event as IHandleMouseDown;
+    let action: Action;
+    switch (handleMouseDown.handle) {
+      case PrincipialWind.NW:
+        action = Action.ScaleNW;
+        break;
+      case PrincipialWind.N:
+        action = Action.ScaleN;
+        break;
+      case PrincipialWind.NE:
+        action = Action.ScaleNE;
+        break;
+      case PrincipialWind.W:
+        action = Action.ScaleW;
+        break;
+      case PrincipialWind.E:
+        action = Action.ScaleE;
+        break;
+      case PrincipialWind.SW:
+        action = Action.ScaleSW;
+        break;
+      case PrincipialWind.S:
+        action = Action.ScaleS;
+        break;
+      case PrincipialWind.SE:
+        action = Action.ScaleSE;
+        break;
+      default:
+        this.currentActionSubj.next(IActionContext.initial());
+    }
+    this.selectedArea = ix;
+    this.selectedAreaChanged.emit(this.selectedArea);
+    const area = this.areas[this.selectedArea];
+    const actionContext: IActionContext = {
+      action: action,
+      selection: this.selectedArea,
+      area: { ...area },
+      originatingEvent: handleMouseDown.attachment
+    }
+    this.currentActionSubj.next(actionContext);
   }
 
   private onActionLayerMouseDown(event: any): void {
-    this.currentActionSubj.next(Action.NoAction);
+    this.currentActionSubj.next(IActionContext.initial());
   }
 
   private onActionLayerMouseOut(event: any): void {
-    this.currentActionSubj.next(Action.NoAction);
+    this.currentActionSubj.next(IActionContext.initial());
   }
 
   private onActionLayerMouseMove(event: any): void {
+    const actionContext =
+      this.updateActionContext(this.currentActionSubj.getValue(), event);
+    this.currentActionSubj.next(actionContext);
   }
 
   private onActionLayerMouseUp(event: any): void {
-    this.currentActionSubj.next(Action.NoAction);
+    const actionContext =
+      this.updateActionContext(this.currentActionSubj.getValue(), event);
+    this.completeAction(actionContext);
+    this.currentActionSubj.next(IActionContext.initial());
+  }
+
+  private updateActionContext(oldContext: IActionContext, event: any): IActionContext {
+    switch (oldContext.action) {
+      case Action.Add:
+        return this.updateAddSCtx(oldContext, event);
+      case Action.Select: // fall through
+      case Action.Move:
+        return this.updateMoveSCtx(oldContext, event);
+      default:
+        return IActionContext.initial();
+    }
+  }
+
+  private updateAddSCtx(oldContext: IActionContext, event: any): IActionContext {
+    const deltaX = oldContext.originatingEvent.x - event.x;
+    const deltaY = oldContext.originatingEvent.y - event.y;
+    const left = Math.min(oldContext.area.x, oldContext.area.x - deltaX);
+    const right = Math.max(oldContext.area.x, oldContext.area.x - deltaX);
+    const top = Math.min(oldContext.area.y, oldContext.area.y - deltaY);
+    const bottom = Math.max(oldContext.area.y, oldContext.area.y - deltaY);
+    this.areas[oldContext.selection] = {x: left,
+      y: top, width: right - left, height: bottom - top};
+    this.areasChanged.emit(this.areas);
+    const retVal: IActionContext = {
+      action: Action.Add,
+      selection: oldContext.selection,
+      area: oldContext.area,
+      originatingEvent: oldContext.originatingEvent
+    }
+    return retVal;
+  }
+
+  private updateMoveSCtx(oldContext: IActionContext, event: any): IActionContext {
+    const deltaX = event.x - oldContext.originatingEvent.x;
+    const deltaY = event.y - oldContext.originatingEvent.y;
+    let x = Math.max(oldContext.area.x + deltaX, 0);
+    let y = Math.max(oldContext.area.y + deltaY, 0);
+    x = Math.min(this.width - oldContext.area.width, x);
+    y = Math.min(this.height - oldContext.area.height, y);
+    this.areas[oldContext.selection] = {x: x, y: y,
+      width: oldContext.area.width, height: oldContext.area.height};
+    this.areasChanged.emit(this.areas);
+    const retVal: IActionContext = {
+      action: Action.Move,
+      selection: oldContext.selection,
+      area: oldContext.area,
+      originatingEvent: oldContext.originatingEvent
+    }
+    return retVal;
+  }
+
+  private completeAction(context: IActionContext): void {
   }
 
   private get actionLayerState(): boolean {
-    return this.currentActionSubj.getValue() !== Action.NoAction;
+    return this.currentActionSubj.getValue().action !== Action.NoAction;
   }
 
   private showHandles(ix: number) : boolean {
