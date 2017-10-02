321d5781095056351ccc1a011c0b27631d5f12fd
Sergei Zykov <sergey.zykov@gmail.com>
2017-10-03 01:33:02 +0300

    Seems like it's working

--- emerald/emerald-angular/src/app/img-region-editor/region-editor.service.ts
+++ emerald/emerald-angular/src/app/img-region-editor/region-editor.service.ts
@@ -27,30 +27,29 @@ import updateRegions from '../backend/updateRegions';
 import { IBusyIndicatorHolder } from '../util/busy-indicator-holder';
 import setBusyIndicator from '../util/setBusyIndicator';
 
+interface IRegionEditorServiceState {
+  readonly node: ITreeNode;
+
+  imageMeta: IImageMeta;
+  imageMetaChanged: EventEmitter<IImageMeta>;
+
+  pageRange: IPageRange;
+  pageRangeChanged: EventEmitter<IPageRange>;
+
+  dimensions: IDimensions;
+  dimensionsChanged: EventEmitter<IDimensions>;
+
+  overviewRegions: Array<IImageRegion>;
+  overviewRegionsChanged: EventEmitter<Array<IImageRegion>>;
+};
+
 @Injectable()
 export class RegionEditorService implements IBusyIndicatorHolder {
   busyIndicator: Promise<any> = Promise.resolve(1);
 
   selectionsSemicolonPageRange: EventEmitter<any> = new EventEmitter<any>();
 
-  imageMeta: IImageMeta = null;
-  imageMetaChanged: EventEmitter<IImageMeta> = new EventEmitter<IImageMeta>();
-
-  pageRange: IPageRange = RegionEditorService._defPageRange;
-  pageRangeChanged: EventEmitter<IPageRange> = new EventEmitter<IPageRange>();
-
-  dimensions: IDimensions = { };
-  dimensionsChanged: EventEmitter<IDimensions> = new EventEmitter<IDimensions>();
-
-  overviewRegions: Array<IImageRegion> = [];
-  overviewRegionsChanged: EventEmitter<Array<IImageRegion>> = new EventEmitter<Array<IImageRegion>>();
-
-  get regionsBySel(): Array<IImageRegion> {
-    const lbound = this.pageRange.page * this.pageRange.count;
-    let ubound = lbound + this.pageRange.count;
-    ubound = Math.min(ubound, this.overviewRegions.length);
-    return this.overviewRegions.slice(lbound, ubound);
-  }
+  private _context: IRegionEditorServiceState = null;
 
   private static readonly _defPageRange: IPageRange = { page: 0, count: 10 };
   private _isNumberRe: RegExp = new RegExp("^\\d+$");
@@ -64,31 +63,112 @@ export class RegionEditorService implements IBusyIndicatorHolder {
 
     this.selectionsSemicolonPageRange.subscribe((pageRangeDict: any) =>
       this.handlePageRange(pageRangeDict));
-
-    this.imageMetaChanged.subscribe((imageMeta: IImageMeta) =>
-      this.handleImageMetaChange(imageMeta));
   }
 
-  private handleImageMetaChange(imageMeta: IImageMeta) {
-    setBusyIndicator(this, Observable.of(1)).subscribe(() => {
-      this.pageRange = RegionEditorService._defPageRange;
-      this.pageRangeChanged.emit(this.pageRange);
+  private handleSelectedNodeChanged(node: ITreeNode) {
+    const that = this._context;
+    if (!that || (node.id !== that.node.id)) {
+      setBusyIndicator(this, Observable.of(1)).subscribe(() => {
+        if (node.type === NodeType.Image) {
+          setBusyIndicator(this, metaFromNode(this._http,
+            this._httpSettings.DefReqOpts, node))
+              .subscribe((imageMeta: IImageMeta) => {
+                this._context = this.makeNewState(node, imageMeta);
+                this.loadRegions(this._context);
+              });
+        } else {
+          this._context = null;
+        }
+      });
+    }
+  }
 
-      this.overviewRegions = [];
-      this.overviewRegionsChanged.emit(this.overviewRegions);
-      if (imageMeta) {
-        setBusyIndicator(this, allRegions(this._http, imageMeta))
-          .subscribe((regions: Array<IImageRegion>) => {
-            this.overviewRegions = regions;
-            this.overviewRegionsChanged.emit(this.overviewRegions);
-          });
-      }
+  private makeNewState(node: ITreeNode, imageMeta: IImageMeta): IRegionEditorServiceState {
+    const retVal : IRegionEditorServiceState = {
+      node: node,
+      imageMeta: imageMeta,
+      imageMetaChanged: new EventEmitter<IImageMeta>(),
+      pageRange: RegionEditorService._defPageRange,
+      pageRangeChanged: new EventEmitter<IPageRange>(),
+      dimensions: { },
+      dimensionsChanged: new EventEmitter<IDimensions>(),
+      overviewRegions: [],
+      overviewRegionsChanged: new EventEmitter<Array<IImageRegion>>()
+    }
+    return retVal;
+  }
+
+  get imageMeta(): IImageMeta {
+    const that = this._context;
+    return that ? that.imageMeta : null;
+  }
+
+  get imageMetaChanged(): EventEmitter<IImageMeta> {
+    const that = this._context;
+    return that ? that.imageMetaChanged : null;
+  }
+
+  get pageRange(): IPageRange {
+    const that = this._context;
+    return that ? that.pageRange : RegionEditorService._defPageRange;
+  }
+
+  get pageRangeChanged(): EventEmitter<IPageRange> {
+    const that = this._context;
+    return that ? that.pageRangeChanged : null;
+  }
+
+  get dimensions(): IDimensions {
+    const that = this._context;
+    return that ? that.dimensions : null;
+  }
+
+  get dimensionsChanged(): EventEmitter<IDimensions> {
+    const that = this._context;
+    return that ? that.dimensionsChanged: null;
+  }
+
+  get overviewRegions(): Array<IImageRegion> {
+    const that = this._context;
+    return that ? that.overviewRegions : null;
+  }
+
+  get overviewRegionsChanged(): EventEmitter<Array<IImageRegion>> {
+    const that = this._context;
+    return that ? that.overviewRegionsChanged : null;
+  }
+
+  get regionsBySel(): Array<IImageRegion> {
+    const that = this._context;
+    if (that) {
+      const lbound = that.pageRange.page * that.pageRange.count;
+      let ubound = lbound + that.pageRange.count;
+      ubound = Math.min(ubound, that.overviewRegions.length);
+      return that.overviewRegions.slice(lbound, ubound);
+    } else {
+      return null;
+    }
+  }
+
+  private loadRegions(context: IRegionEditorServiceState) {
+    setBusyIndicator(this, Observable.of(1)).subscribe(() => {
+      context.pageRange = RegionEditorService._defPageRange;
+      context.pageRangeChanged.emit(context.pageRange);
+
+      context.overviewRegions = [];
+      context.overviewRegionsChanged.emit(context.overviewRegions);
+      setBusyIndicator(this, allRegions(this._http, context.imageMeta))
+        .subscribe((regions: Array<IImageRegion>) => {
+          context.overviewRegions = regions;
+          context.overviewRegionsChanged.emit(context.overviewRegions);
+        });
     });
   }
 
   private handlePageRange(pageRangeDict: any) {
+    const that = this._context;
     setBusyIndicator(this, Observable.of(1)).subscribe(() => {
-      let newPageRange: IPageRange = this.pageRange;
+      let newPageRange: IPageRange = that.pageRange;
       if (pageRangeDict.page && this._isNumberRe.test(pageRangeDict.page)) {
         newPageRange.page = parseInt(pageRangeDict.page);
       }
@@ -97,75 +177,64 @@ export class RegionEditorService implements IBusyIndicatorHolder {
         newPageRange.count = parseInt(pageRangeDict.count);
       }
 
-      newPageRange.numPages = Math.ceil(this.overviewRegions.length / this.pageRange.count);
+      newPageRange.numPages = Math.ceil(that.overviewRegions.length / that.pageRange.count);
 
-      this.pageRange = newPageRange;
-      this.pageRangeChanged.emit(this.pageRange);
-    });
-  }
-
-  private handleSelectedNodeChanged(node: ITreeNode) {
-    setBusyIndicator(this, Observable.of(1)).subscribe(() => {
-      if (node.type === NodeType.Image) {
-        setBusyIndicator(this, metaFromNode(this._http,
-          this._httpSettings.DefReqOpts, node))
-            .subscribe((imageMeta: IImageMeta) => {
-              this.imageMeta = imageMeta;
-              this.imageMetaChanged.emit(this.imageMeta);
-            });
-      } else {
-        this.imageMeta = null;
-        this.imageMetaChanged.emit(this.imageMeta);
-      }
+      that.pageRange = newPageRange;
+      that.pageRangeChanged.emit(that.pageRange);
     });
   }
 
   get imageHref(): string {
-    return '/emerald/blobs/' + `${this.imageMeta.aquamarineId}`
-      + `?rot=${Rotation[this.imageMeta.rotation]}`
+    const that = this._context;
+    return '/emerald/blobs/' + `${that.imageMeta.aquamarineId}`
+      + `?rot=${Rotation[that.imageMeta.rotation]}`
   }
 
   rotateCW(): void {
+    const that = this._context;
     setBusyIndicator(this, rotateCW(this._http,
-                                   this._httpSettings.DefReqOpts,
-                                   this.imageMeta))
+                                    this._httpSettings.DefReqOpts,
+                                    that.imageMeta))
       .subscribe(im => {
-        this.imageMeta = im;
-        this.imageMetaChanged.emit(this.imageMeta);
+        that.imageMeta = im;
+        that.imageMetaChanged.emit(that.imageMeta);
       });
   }
 
   rotateCCW(): void {
+    const that = this._context;
     setBusyIndicator(this, rotateCCW(this._http,
-                                    this._httpSettings.DefReqOpts,
-                                    this.imageMeta))
+                                     this._httpSettings.DefReqOpts,
+                                     that.imageMeta))
       .subscribe(im => {
-        this.imageMeta = im;
-        this.imageMetaChanged.emit(this.imageMeta);
+        that.imageMeta = im;
+        that.imageMetaChanged.emit(that.imageMeta);
       });
   }
 
   saveRegions(regions: Array<IImageRegion>): void {
-    const scope = () => allRegions(this._http, this.imageMeta);
+    const that = this._context;
+    const scope = () => allRegions(this._http, that.imageMeta);
     setBusyIndicator(this, updateRegions(this._http, this._httpSettings.DefReqOpts,
-      this.imageMeta, scope, regions))
+      that.imageMeta, scope, regions))
         .subscribe(regions => {
-          this.overviewRegions = regions;
-          this.overviewRegionsChanged.emit(this.overviewRegions);
+          that.overviewRegions = regions;
+          that.overviewRegionsChanged.emit(that.overviewRegions);
         });
   }
 
   updateDimensions(naturalWidth?: number, naturalHeight?: number,
     clientWidth?: number, clientHeight?: number)
   {
+    const that = this._context;
     setBusyIndicator(this, Observable.of(1)).subscribe(() => {
-      this.dimensions = {
+      that.dimensions = {
          naturalWidth: naturalWidth,
          naturalHeight: naturalHeight,
          clientWidth: clientWidth,
          clientHeight: clientHeight
       };
-      this.dimensionsChanged.emit(this.dimensions);
+      that.dimensionsChanged.emit(that.dimensions);
     });
   }
 }
