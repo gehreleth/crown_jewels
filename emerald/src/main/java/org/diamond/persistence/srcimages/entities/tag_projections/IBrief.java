d1ea244e19c71fb5a35f89badb350c5553c87ead
Sergei Zykov <sergey.zykov@gmail.com>
2017-10-18 20:47:45 +0300

    New projections

--- emerald/src/main/java/org/diamond/persistence/srcimages/entities/tag_projections/IBrief.java
+++ emerald/src/main/java/org/diamond/persistence/srcimages/entities/tag_projections/IBrief.java
@@ -3,7 +3,7 @@ package org.diamond.persistence.srcimages.entities.tag_projections;
 import org.diamond.persistence.srcimages.entities.Tag;
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(name = "name_only", types = Tag.class)
-public interface INameOnly {
+@Projection(name = "brief", types = Tag.class)
+public interface IBrief {
     String getName();
 }
