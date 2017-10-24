f23b2eb00e9f7cd95f828ac0e463af1c1c84e616
Sergei Zykov <sergey.zykov@gmail.com>
2017-10-24 20:57:38 +0300

    Fixed another regression

--- emerald/emerald-angular/src/app/backend/extendRegionsWithTags.ts
+++ emerald/emerald-angular/src/app/backend/extendRegionsWithTags.ts
@@ -11,16 +11,20 @@ import 'rxjs/add/observable/forkJoin';
 export default function extendRegionsWithTags(http: Http, regions: Array<IImageRegion>)
   : Observable<Array<ITaggedImageRegion>>
 {
-  return Observable.forkJoin(regions.map(r => http.get(`${r.href}?projection=brief`)
-    .map((rsp: Response) => {
-      const dict = rsp.json();
-      return { region: r, tagsHref: new URL(dict._links.tags.href).pathname };
-    }))).concatMap(pairs => Observable.forkJoin(pairs.map(pair =>
-      http.get(`${pair.tagsHref}?projection=full`).map(rsp => {
-        let retVal: ITaggedImageRegion = {
-          ...pair.region, tags: ITag.fromEmbedded(rsp.json())
-        };
-        return retVal;
-      })
-    )));
+  if (regions && regions.length > 0) {
+    return Observable.forkJoin(regions.map(r => http.get(`${r.href}?projection=brief`)
+      .map((rsp: Response) => {
+        const dict = rsp.json();
+        return { region: r, tagsHref: new URL(dict._links.tags.href).pathname };
+      }))).concatMap(pairs => Observable.forkJoin(pairs.map(pair =>
+        http.get(`${pair.tagsHref}?projection=full`).map(rsp => {
+          let retVal: ITaggedImageRegion = {
+            ...pair.region, tags: ITag.fromEmbedded(rsp.json())
+          };
+          return retVal;
+        })
+      )));
+  } else {
+    return Observable.of([]);
+  }
 }
